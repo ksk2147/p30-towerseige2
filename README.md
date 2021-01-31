8483b6bc356c765489bde6fc1887c8c65f115fe1
ksk2147 <72691651+ksk2147@users.noreply.github.com>
2021-01-31 12:41:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# p30v
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ circle.js
@@ -0,0 +1,39 @@
+class Circle
+{
+	constructor(x,y,r){
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+			//this.image = loadImage("hexagon.png")
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+
+
+	display()
+	{
+			
+			var circle1pos=this.body.position;		
+
+			//image(this.image, this.body.position.x, this.body.position.y, )
+
+			push()
+			translate(circle1pos.x, circle1pos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill("blue")
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
